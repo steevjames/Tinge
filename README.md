101909303df5f347a20d2558c4117de8b2fc233b
Steev James <steevjames11@gmail.com>
2018-08-15 18:23:05 +0530

    Made it Cordova Project

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Color-Guessing-Game
+# Color-Guessing-Game : 
 A simple Color Guessing game using HTML, CSS and JS
---
+++ platforms/browser/www/script.js
@@ -0,0 +1,92 @@
+var colors = [];
+var numSquares = 6;
+var sqrList = document.querySelectorAll(".square");
+var pickedColor;
+var colorDisplay = document.querySelector("#curColor");
+var resultDisplay = document.querySelector("#result");
+var header = document.querySelector("#header");
+var reset = document.querySelector("#reset");
+var score = 0;
+var total = 0;
+var scoreDisplay = document.querySelector("#won");
+var totalDisplay = document.querySelector("#total");
+
+init();
+
+function init() {
+	setUpSquareBox();
+	resetPage();
+}
+
+function setUpSquareBox() {
+	for (var i = 0; i < sqrList.length; i++) {
+		sqrList[i].addEventListener("click", function () {
+			total++;
+			if (this.style.backgroundColor != pickedColor) {
+				alert("Wrong choice!");
+				this.style.backgroundColor = "black";
+				resultDisplay.textContent = "Try Again!"
+			}
+			else {
+				score++;
+				alert("correct");
+				resultDisplay.textContent = "Correct!"
+				reset.textContent = "Play Again?";
+				changeColors(pickedColor);
+				resetPage();
+				
+			}
+			scoreDisplay.textContent = score;
+			totalDisplay.textContent = total;
+		});
+	}
+}
+
+
+function resetPage() {
+	colors = generateRandomColors(numSquares);
+	pickedColor = pickColor();
+	colorDisplay.textContent = pickedColor;
+	for (var i = 0; i < sqrList.length; i++) {
+		if (colors[i]) {
+			sqrList[i].style.display = "block";
+			sqrList[i].style.backgroundColor = colors[i];
+		}
+		else
+			sqrList[i].style.display = "none";
+	}
+	//header.style.backgroundColor = "steelblue";
+	resultDisplay.textContent = "";
+	reset.textContent = "Shuffle Colors";
+}
+
+
+
+reset.addEventListener("click", function () {
+	resetPage();
+});
+
+
+function changeColors(color) {
+	for (var i = 0; i < sqrList.length; i++) {
+		sqrList[i].style.backgroundColor = color;
+	}
+	header.style.backgroundColor = color;
+}
+
+function pickColor() {
+	var rand = Math.floor(Math.random() * colors.length);
+	return colors[rand];
+}
+
+function generateRandomColors(num) {
+	var arr = [];
+	for (var i = 0; i < num; i++) {
+		var r = Math.floor(Math.random() * 256);
+		var g = Math.floor(Math.random() * 256);
+		var b = Math.floor(Math.random() * 256);
+		var res = "rgb(" + r + ", " + g + ", " + b + ")";
+		arr.push(res);
+	}
+	return arr;
+}
